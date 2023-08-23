a6bc4157f5c79ac3978a0e245fdaef7b697b0566
Violetta Haveman <120049344+vihaveman@users.noreply.github.com>
2023-08-22 23:49:45 -0400

    Update dashboard.js

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,71 +1,39 @@
-d3.json("/stockinfo").then(data => {
+d3.json("/production").then(data => {
     console.log(data);
 
     // Populate the ticker dropdown with the unique tickers from the data.
-    const tickers = [...new Set(data.map(item => item.Ticker))];
-    const tickerDropdown = d3.select("#tickerDropdown");
-    tickers.forEach(ticker => {
-        tickerDropdown.append("option").text(ticker);
+    const products = [...new Set(data.map(item => item.Product))];
+    const productDropdown = d3.select("#productDropdown");
+    products.forEach(ticker => {
+        productDropdown.append("option").text(product);
     });
 
     // When a dropdown value is changed, update the visualizations.
-    d3.select("#tickerDropdown").on("change", updateVisualizations);
+    d3.select("#productDropdown").on("change", updateVisualizations);
 
     function updateVisualizations() {
-        const selectedTicker = tickerDropdown.node().value;
+        const selectedProduct = productDropdown.node().value;
 
         // Filter the data based on the selected ticker.
-        const filteredData = data.filter(item => item.Ticker === selectedTicker);
+        const filteredData = data.filter(item => item.Product === selectedProduct);
 
         // Get the container to add the table to
-        const stockInfo = d3.select("#stockInfo");
+        const product = d3.select("#product");
 
         // Clear the previous contents.
-        stockInfo.html(""); 
+        product.html(""); 
 
         // Check if filteredData is not empty
         if (filteredData.length > 0) {
             // Create the table
-            createTable(filteredData, stockInfo);
+            createTable(filteredData, product);
         } else {
             // Handle the case where filteredData is empty, e.g., display a message
-            console.log("No data matches the selected ticker.");
+            console.log("No data matches the selected product.");
         }
     }
 
     // Call the function to update the visualizations initially.
     updateVisualizations();
 
-    function createTable(data, container) {
-        // If there's already a table remove it
-        container.select("table").remove();
-    
-        let table = container.append("table").attr("class", "green-table");
-        let tbody = table.append("tbody");
-    
-        // Exclude the "_id" key-value pair
-        let filteredData = data.map(d => {
-            delete d["_id"];
-            return d;
-        });
-    
-        // Transpose the data
-        let transposedData = Object.keys(filteredData[0]).map(key => {
-            return [key, ...filteredData.map(d => d[key])];
-        });
-    
-        // Append the rows
-        let rows = tbody.selectAll("tr")
-            .data(transposedData).enter()
-            .append("tr");
-    
-        // Append the cells
-        let cells = rows.selectAll("td")
-            .data(d => d).enter()
-            .append("td").text(d => d);
-    }
-        
-    
-}).catch(error => console.log(error));
-
-
+   
