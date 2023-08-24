6752feda497128a62f170d3a9ea5d101bd476b0a
Violetta Haveman <120049344+vihaveman@users.noreply.github.com>
2023-08-24 00:47:06 -0400

    Update employee.js

--- static/js/employee.js
+++ static/js/employee.js
@@ -1,71 +1,93 @@
-// Load data Flask app
-d3.json('/employee').then(data => {
+$(document).ready(function() {
+  // Load data from Flask app
+  d3.json('/employee').then(data => {
     // Process data and cluster based on Satisfaction
-    const clusters = {};
-  
+    const clusters = {
+      "below3": {},
+      "between3and4": {},
+      "above4": {}
+    };
+
     data.forEach(item => {
-      const department = item.Department;
       const satisfaction = parseFloat(item.Satisfaction);
-  
-      if (!clusters[satisfaction]) {
-        clusters[satisfaction] = [];
+      const department = item.Department;
+      const gender = item.Gender;
+
+      if (satisfaction < 3) {
+        if (!clusters["below3"][department]) {
+          clusters["below3"][department] = {};
+        }
+        if (!clusters["below3"][department][gender]) {
+          clusters["below3"][department][gender] = 0;
+        }
+        clusters["below3"][department][gender]++;
+      } else if (satisfaction >= 3 && satisfaction <= 4) {
+        if (!clusters["between3and4"][department]) {
+          clusters["between3and4"][department] = {};
+        }
+        if (!clusters["between3and4"][department][gender]) {
+          clusters["between3and4"][department][gender] = 0;
+        }
+        clusters["between3and4"][department][gender]++;
+      } else {
+        if (!clusters["above4"][department]) {
+          clusters["above4"][department] = {};
+        }
+        if (!clusters["above4"][department][gender]) {
+          clusters["above4"][department][gender] = 0;
+        }
+        clusters["above4"][department][gender]++;
       }
-  
-      clusters[satisfaction].push(department);
     });
-  
-    // Convert clusters to an array of objects
-    const clusterArray = Object.keys(clusters).map(satisfaction => ({
-      satisfaction: parseFloat(satisfaction),
-      departments: clusters[satisfaction]
-    }));
-  
-    // Create a simple bar chart using D3.js
-    const margin = { top: 20, right: 30, bottom: 40, left: 40 };
-    const width = 600 - margin.left - margin.right;
-    const height = 400 - margin.top - margin.bottom;
-  
-    const svg = d3.select('#clusterContainer')
-      .append('svg')
-      .attr('width', width + margin.left + margin.right)
-      .attr('height', height + margin.top + margin.bottom)
-      .append('g')
-      .attr('transform', `translate(${margin.left},${margin.top})`);
-  
-    const xScale = d3.scaleBand()
-      .domain(clusterArray.map(d => d.satisfaction))
-      .range([0, width])
-      .padding(0.1);
-  
-    const yScale = d3.scaleLinear()
-      .domain([0, d3.max(clusterArray, d => d.departments.length)])
-      .range([height, 0]);
-  
-    svg.selectAll('.bar')
-      .data(clusterArray)
-      .enter().append('rect')
-      .attr('class', 'bar')
-      .attr('x', d => xScale(d.satisfaction))
-      .attr('y', d => yScale(d.departments.length))
-      .attr('width', xScale.bandwidth())
-      .attr('height', d => height - yScale(d.departments.length));
-  
-    svg.append('g')
-      .attr('class', 'x-axis')
-      .attr('transform', `translate(0,${height})`)
-      .call(d3.axisBottom(xScale));
-  
-    svg.append('g')
-      .attr('class', 'y-axis')
-      .call(d3.axisLeft(yScale));
-  
-    svg.append('text')
-      .attr('x', width / 2)
-      .attr('y', -10)
-      .attr('text-anchor', 'middle')
-      .text('Department Clustering by Satisfaction');
+
+    // Handle dropdown change event
+    $('#scoreDropdown').change(function() {
+      const selectedScore = $(this).val();
+      populateTable(selectedScore);
+    });
+
+    // Function to populate the table
+    function populateTable(score) {
+      const tableContainer = $('#tableContainer');
+      tableContainer.empty();
+
+      const table = $('<table class="table table-bordered table-hover"></table>');
+      const thead = $('<thead></thead>').appendTo(table);
+      const tbody = $('<tbody></tbody>').appendTo(table);
+
+      // Create table header
+      const headerRow = $('<tr></tr>').appendTo(thead);
+      headerRow.append('<th>Department</th>');
+      headerRow.append('<th>Gender</th>');
+      headerRow.append('<th>Count</th>');
+
+      // Populate table rows
+      for (const department in clusters[score]) {
+        for (const gender in clusters[score][department]) {
+          const count = clusters[score][department][gender];
+          const row = $('<tr></tr>').appendTo(tbody);
+
+          // Add classes based on gender for styling
+          if (gender === 'M') {
+            row.addClass('table-info'); // Blue for male
+          } else if (gender === 'F') {
+            row.addClass('table-pink'); // Pink for female
+          }
+
+          row.append(`<td>${department}</td>`);
+          row.append(`<td>${gender}</td>`);
+          row.append(`<td>${count}</td>`);
+        }
+      }
+
+      table.appendTo(tableContainer);
+    }
+
+    // Initial population of the table
+    populateTable("below3");
   });
-  
+});
+
   
 
 
